dcf8a6cf228a1633d5af7513c927b4cdfbe18994
YOYO8 <violette.danjou@hotmail.fr>
2020-10-22 13:08:51 +0200

    Premier enregistrement

--- index.php
+++ index.php
@@ -1,48 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Super blog</title>
-</head>
-<body>
-    <h1>Mon super blog !</h1>
+<?php
+require('model.php');
 
-    <p>
-        Derniers billets du blog : <br/>
-        <?php
-        try
-        {
-            $bdd = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root', 
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }
-        catch(Exception $e)
-        {
-                die('Erreur : '.$e->getMessage());
-        }
+$req = getBillets();
 
-        $billets = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr  FROM billets ORDER BY date_creation DESC LIMIT 0,5');
-
-        while ($donnees = $billets->fetch())
-        {
-        ?>
-            <div class="news">
-                <h3>
-                    <?php echo htmlspecialchars($donnees['titre']); ?>
-                    <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-                </h3>
-                
-                <p>
-                <?php
-                // On affiche le contenu du billet
-                echo nl2br(htmlspecialchars($donnees['contenu']));
-                ?>
-                <br />
-                <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
-                </p>
-            </div>
-        <?php
-        }
-            $billets->closeCursor();
-        ?>
-    </p>
-</body>
-</html>
+require('view.php');
+?>

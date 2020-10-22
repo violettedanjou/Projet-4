8266b27b885f95a1b36ff610c0f01baeb2cce5d9
YOYO8 <violette.danjou@hotmail.fr>
2020-10-22 15:13:57 +0200

    Ajouts de deux nouvelles fonctions pour récupérer les commentaires

--- model.php
+++ model.php
@@ -3,15 +3,51 @@ function getBillets()
 {
 	try
 	{
-	    $bdd = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
+	    $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
 	}
 	catch(Exception $e)
 	{
 	    die('Erreur : '.$e->getMessage());
 	}
 
-	$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets ORDER BY creation_date DESC LIMIT 0, 5');
+	$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets ORDER BY creation_date DESC LIMIT 0, 5');
 
 	return $req;
 }
+
+function getPost($postId)
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
+
+function getComments($postId)
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM commentaires WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
 ?>

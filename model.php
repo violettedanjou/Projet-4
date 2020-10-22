b27965ff0f2cc50b43964cd512070eb79ab0b53a
YOYO8 <violette.danjou@hotmail.fr>
2020-10-22 15:16:38 +0200

    Ajout de dbConnect() pour réduire les try/catch

--- model.php
+++ model.php
@@ -1,15 +1,7 @@
 <?php
 function getBillets()
 {
-	try
-	{
-	    $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
-	}
-	catch(Exception $e)
-	{
-	    die('Erreur : '.$e->getMessage());
-	}
-
+	$db = dbConnect();
 	$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets ORDER BY creation_date DESC LIMIT 0, 5');
 
 	return $req;
@@ -17,15 +9,7 @@ function getBillets()
 
 function getPost($postId)
 {
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
-
+    $db = dbConnect();
     $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM billets WHERE id = ?');
     $req->execute(array($postId));
     $post = $req->fetch();
@@ -35,19 +19,23 @@ function getPost($postId)
 
 function getComments($postId)
 {
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
-
+    $db = dbConnect();
     $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM commentaires WHERE post_id = ? ORDER BY comment_date DESC');
     $comments->execute(array($postId));
 
     return $comments;
 }
 
+function dbConnect()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=TP_commentaires;charset=utf8', 'root', 'root');
+        return $db;
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+}
 ?>

7f87308bc5e1f7a4d01ede32b384632220dedba1
YOYO8 <violette.danjou@hotmail.fr>
2020-10-23 11:17:31 +0200

    Creation Template

--- view.php
+++ view.php
@@ -1,37 +1,31 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="commentaires.css" rel="stylesheet" /> 
-    </head>
+<!-- Affiche la liste des derniers billets -->
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
+
+
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            <em>le <?= $data['creation_date_fr'] ?></em>
+        </h3>
         
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-        
-        <?php
-        while ($data = $posts->fetch())
-        {
-        ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($data['title']); ?>
-                <em>le <?php echo $data['creation_date_fr']; ?></em>
-            </h3>
-            
-            <p>
-            <?php
-            echo nl2br(htmlspecialchars($data['content']));
-            ?>
+        <p>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
             <br />
             <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
-            </p>
-        </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
-    </body>
-</html>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?> <!-- On récupère le code HTML, avec ob_start() et ob_get_clean(), et on le met dans la variable $content qui s'affichera dans le template.php-->
+
+<?php require('template.php'); ?>

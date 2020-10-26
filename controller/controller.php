73dfea31591e210f2f97a6438d62af040770cef9
YOYO8 <violette.danjou@hotmail.fr>
2020-10-26 13:11:52 +0100

    Mise en place de la POO

--- controller/controller.php
+++ controller/controller.php
@@ -1,30 +1,36 @@
 <?php
 
-require('model/model.php');
+// Chargement des classes
+require_once('model/PostManager.php'); // require_once permet de ne pas charger deux fois la classe
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
-    $posts = getPosts();
+    $postManager = new PostManager(); // Création d'un objet
+    $posts = $postManager->getPosts(); // Appel d'une fonction, getPosts(), dans cet objet
 
     require('view/listPostsView.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
 
     require('view/postView.php');
 }
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
-        //die('Impossible d\'ajouter le commentaire !');
-        throw new Exception("Impossible d\'ajouter le commentaire !"); // on remplace le die par throw qui remontera une erreur, s'il y en a une, dans le catch du routeur
-        
+        throw new Exception('Impossible d\'ajouter le commentaire !');
     }
     else {
         header('Location: index.php?action=post&id=' . $postId);

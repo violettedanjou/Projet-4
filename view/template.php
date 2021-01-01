0c221cdc47961d346c78f9199e6a038314fe9786
YOYO8 <violette.danjou@hotmail.fr>
2021-01-01 17:21:08 +0100

    CSS : ajout background-image + changement couleur h1

--- view/template.php
+++ view/template.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   		<script>tinymce.init({selector:'textarea'});</script>
-  		
+
         <title><?= $title ?></title>
         <link href="public/commentaires.css" rel="stylesheet" /> 
     </head>
@@ -21,17 +21,17 @@
 				<nav>
 					<ul>
 						<?php if(isset($_SESSION['pseudo'])) { ?>	
-							<li class="li" id="button_deconnexion"><a href="index.php?action=validDeconnexion">Déconnexion</a></li>
+							<li class="li" id="button_deconnexion"><a href="index.php?action=validDeconnexion">DECONNEXION</a></li>
 
 							<?php if($_SESSION['admin'] != 0) { ?>
-								<li class="li" id="button_admin"><a href="index.php?action=afficheAdmin">Administration</a></li>
+								<li class="li" id="button_admin"><a href="index.php?action=afficheAdmin">ADMINISTRATION</a></li>
 							<?php }
 						}
 						
 						else { 
 						?> 
-							<li class="li" id="button_inscription"><a href="index.php?action=afficheInscription">Créer un compte</a></li>
-							<li class="li" id="button_connexion"><a href="index.php?action=afficheConnection">Connexion</a></li>
+							<li class="li" id="button_inscription"><a href="index.php?action=afficheInscription">CREER UN COMPTE</a></li>
+							<li class="li" id="button_connexion"><a href="index.php?action=afficheConnection">CONNEXION</a></li>
 						<?php }?>
 					</ul>
 				</nav>
